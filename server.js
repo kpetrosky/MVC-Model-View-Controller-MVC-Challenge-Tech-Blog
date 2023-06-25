8f92c157101de82e7ebb76a68bc8646ba5c1f7f6
kpetrosky <kpetrosky36@gmail.com>
2023-06-24 19:48:21 -0700

    working on getting deployed to heroku

--- server.js
+++ server.js
@@ -1,67 +1,42 @@
-//npm i inquirer@8.2.4
-//npm i dotenv 
-//npm init -y
-//npm i express
-//mysql -u root -p
-//SOURCE schema.sql;
-//SOURCE insert-schema.sql;
-//SHOW databases;
-//SHOW tables;
-//node server.js
-//gonna use insomnia
-//npx sequelize-cli migration:generate --name add_name_to_users
-//npm i startbootstrap-clean-blog
-//npm i --save-dev nodemon
-//npm install express-handlebars
-//npm install express-session
-//npm i bcrypt
-//npm i sequelize
-
-
-
-const path = require('path');
 const express = require('express');
-// const api = require('./controllers/api');
-const exphbs = require("express-handlebars");
-const session = require("express-session");
-
-
-
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
-
-const sequelize = require('./config/connection');
+const mysql = require('mysql');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
-const hbs = exphbs.create({ });
-
-const sess = {
-    secret: 'Super secret secret',
-    cookie: {
-        maxAge: 300000,
-        httpOnly: true,
-        secure: false,
-        sameSite: 'strict',
-    },
-    resave: false,
-    saveUninitialized: true,
-    db: sequelize
-};
-app.use(session(sess));
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-app.use(express.json());
+app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: 'your-jawsdb-hostname',
+  port: 'your-jawsdb-port',
+  user: 'your-jawsdb-username',
+  password: 'your-jawsdb-password',
+  database: 'your-jawsdb-database-name'
+});
+require('./routes/htmlRoutes')(app);
 
-app.use(routes);
-
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+app.get('/your-route', (req, res) => {
+  // Use the connection pool for executing queries
+  pool.query('SELECT * FROM your_table', (err, results) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+    console.log('Query results:', results);
+    res.json(results);
+  });
 });
 
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error connecting to JawsDB:', err);
+    return;
+  }
+  console.log('Connected to JawsDB MySQL database!');
+  connection.release();
+});
 
+app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
